49141c0d10af1659b6e81de8eba3b3fe4c7399aa
manU <manuelcatun10@gmail.com>
2026-07-14 20:01:10 -0600

    Constructor del cliente y funciones para cargar archivos

--- src/structures/Client.js
+++ src/structures/Client.js
@@ -39,10 +39,109 @@ module.exports = class extends Client {
             type: ActivityType[process.env.ACTIVITY_TYPE],
           },
         ],
-        status: "online",
+        status: PresenceUpdateStatus.DoNotDisturb,
       },
     },
   ) {
     super(options);
+
+    this.slashCommands = new Collection();
+    this.slashArray = [];
+
+    this.utils = new BotUtils(this);
+
+    this.start();
+  }
+
+  async start() {
+    await this.loadEvents();
+    await this.loadHandlers();
+    await this.loadSlashCommands();
+
+    this.login(process.env.TOKEN);
+  }
+
+  async loadEvents() {
+    console.log("🔔 Cargando eventos...".yellow);
+    this.removeAllListeners();
+
+    const rutaArchivos = await this.utils.loadFiles("/src/events");
+    if (rutaArchivos.length) {
+      rutaArchivos.forEach((archivo) => {
+        try {
+          const evento = require(archivo);
+          const nombreEvento = archivo
+            .split("\\")
+            .pop()
+            .split("/")
+            .pop()
+            .split(".")[0];
+          this.on(nombreEvento, evento.bind(null, this));
+        } catch (e) {
+          console.log(`🚨 Error al cargar el archivo (${archivo})`.bgRed);
+          console.log(e);
+        }
+      });
+    }
+
+    console.log(`🔔 ${rutaArchivos.length} Eventos Cargados`.brightGreen.bold);
+  }
+
+  async loadHandlers() {
+    console.log("📂 Cargando Handlers...".yellow);
+
+    const rutaArchivos = await this.utils.loadFiles("/src/handlers");
+    if (rutaArchivos.length) {
+      rutaArchivos.forEach((archivo) => {
+        try {
+          require(archivo)(this);
+        } catch (e) {
+          console.log(`🚨 Error al cargar el archivo (${archivo})`.bgRed);
+          console.log(e);
+        }
+      });
+    }
+
+    console.log(`📂 ${rutaArchivos.length} Handlers Cargados`.brightGreen.bold);
+  }
+
+  async loadSlashCommands() {
+    console.log("⚙️ Cargando Slash Commands...".yellow);
+    this.slashCommands.clear();
+    this.slashArray = [];
+
+    const rutaArchivos = await this.utils.loadFiles("/src/slashCommands");
+    if (rutaArchivos.length) {
+      rutaArchivos.forEach((archivo) => {
+        try {
+          const comando = require(archivo);
+          const nombreComando = archivo
+            .split("\\")
+            .pop()
+            .split("/")
+            .pop()
+            .split(".")[0];
+
+          comando.CMD.name = nombreComando;
+          if (nombreComando) this.slashCommands.set(nombreComando, comando);
+          this.slashArray.push(comando.CMD.toJSON());
+        } catch (e) {
+          console.log(`🚨 Error al cargar el archivo (${archivo})`.bgRed);
+          console.log(e);
+        }
+      });
+    }
+
+    console.log(
+      `⚙️ ${rutaArchivos.length} Slash Commands Cargados`.brightGreen.bold,
+    );
+
+    if (this?.application?.commands) {
+      await this.application.commands.set(this.slashArray);
+      console.log(
+        `⚙️ ${this.slashArray.length} Slash Commands Registrados`.brightGreen
+          .bold,
+      );
+    }
   }
 };
